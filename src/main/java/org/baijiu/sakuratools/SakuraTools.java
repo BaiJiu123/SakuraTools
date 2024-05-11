2264a6dfccec47b1f7d0cfb83e0f50c8b39873fa
BaiJiu123 <sakura_baijiu@outlook.com>
2024-05-11 11:05:14 +0800

    改善 commands

--- src/main/java/org/baijiu/sakuratools/SakuraTools.java
+++ src/main/java/org/baijiu/sakuratools/SakuraTools.java
@@ -33,29 +33,38 @@ public final class SakuraTools extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
         String playerName = sender.getName();
-        GemsEconomyAPI api = new me.xanium.gemseconomy.api.GemsEconomyAPI();
+        GemsEconomyAPI ecoapi = new me.xanium.gemseconomy.api.GemsEconomyAPI();
         if (label.equalsIgnoreCase("itemid") && sender instanceof Player) {
             String ItemType = player.getInventory().getItemInHand().getType().toString();
             sender.sendMessage("§8[§f❀§8] §f当前手持物品名为 §e" + ItemType);
             return true;
         }
-        if (label.equalsIgnoreCase("gm1") && sender instanceof Player && player.hasPermission("sakuratools.admin")) {
-            player.setGameMode(GameMode.CREATIVE);
-            player.sendMessage("§8[§f❀§8] §7已将您的游戏模式设置为 §e创造");
-            return true;
-        }
-        if (label.equalsIgnoreCase("gm0") && sender instanceof Player && player.hasPermission("sakuratools.admin")) {
-            player.setGameMode(GameMode.SURVIVAL);
-            player.sendMessage("§8[§f❀§8] §7已将您的游戏模式设置为 §e生存");
-            return true;
-        }
-        if (label.equalsIgnoreCase("gm3") && sender instanceof Player && player.hasPermission("sakuratools.admin")) {
-            player.setGameMode(GameMode.SPECTATOR);
-            player.sendMessage("§8[§f❀§8] §7已将您的游戏模式设置为 §e旁观");
-            return true;
+        if (label.equalsIgnoreCase("gm") && sender instanceof Player && player.hasPermission("sakuratools.admin")) {
+            if (args.length == 1 && args[0].equalsIgnoreCase("1")) {
+                player.setGameMode(GameMode.CREATIVE);
+                player.sendMessage("§8[§f❀§8] §7已将您的游戏模式设置为 §e创造");
+                return true;
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("0")) {
+                player.setGameMode(GameMode.SURVIVAL);
+                player.sendMessage("§8[§f❀§8] §7已将您的游戏模式设置为 §e生存");
+                return true;
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("3")) {
+                player.setGameMode(GameMode.SPECTATOR);
+                player.sendMessage("§8[§f❀§8] §7已将您的游戏模式设置为 §e旁观");
+                return true;
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("2")) {
+                player.setGameMode(GameMode.ADVENTURE);
+                player.sendMessage("§8[§f❀§8] §7已将您的游戏模式设置为 §e冒险");
+                return true;
+            } else {
+                player.sendMessage("§8[§f❀§8] §c用法: /gm < 0 | 1 | 2 | 3 >");
+                return false;
+            }
+
         }
 
-        if (label.equalsIgnoreCase("fix") && sender instanceof Player && api.getBalance(player.getUniqueId(), api.getCurrency("SkpCoins")) >= 2000) {
+
+        if (label.equalsIgnoreCase("fix") && sender instanceof Player && ecoapi.getBalance(player.getUniqueId(), ecoapi.getCurrency("SkpCoins")) >= 2000) {
             if (!(player.getItemInHand().getDurability() == (short) 0)) {
                 player.getItemInHand().setDurability((short)0);
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco take " + playerName + " 2000 SkpCoins");
@@ -65,7 +74,7 @@ public final class SakuraTools extends JavaPlugin implements Listener {
             } else {
                 player.sendMessage("§8[§f❀§8] §7该物品无需修复!");
             }
-        } else if (api.getBalance(player.getUniqueId(), api.getCurrency("SkpCoins")) < 2000) {
+        } else if (ecoapi.getBalance(player.getUniqueId(), ecoapi.getCurrency("SkpCoins")) < 2000) {
             player.sendMessage("§8[§f❀§8] §7你没有足够的货币来修复!");
         }
 
